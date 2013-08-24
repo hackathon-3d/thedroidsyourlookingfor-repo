496c26d9c63edbfce250546b113e164a7adf6d80
Stephen Quick <squickone@gmail.com>
2013-08-24 05:07:46 -0400

    recipe updates

--- src/com/sparc/BoozeChoose/Activity/Recipe.java
+++ src/com/sparc/BoozeChoose/Activity/Recipe.java
@@ -1,7 +1,11 @@
 package com.sparc.BoozeChoose.Activity;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.sparc.BoozeChoose.R;
 
 /**
  * User: stephen quick
@@ -13,5 +17,12 @@ public class Recipe extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        View bgImage = findViewById(R.id.recipeBackground);
+        Drawable background = bgImage.getBackground();
+        background.setAlpha(80);
+
+        TextView recipeText = (TextView) findViewById(R.id.recipeText);
+        recipeText.setText("Hello World");
     }
 }
