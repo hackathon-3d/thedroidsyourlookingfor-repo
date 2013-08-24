2a8ab09c5ad0b524110bb87ee9c3ddc96997c054
Whitney Champion <whitney.champion@sparcedge.com>
2013-08-23 23:59:39 -0400

    button listeners for ingredients

--- src/com/sparc/BoozeChoose/Activity/BoozeChoose.java
+++ src/com/sparc/BoozeChoose/Activity/BoozeChoose.java
@@ -31,13 +31,49 @@ public class BoozeChoose extends Activity {
             throw new Error("Unable to create database");
         }
 
-        // button listener for contests
+        // button listener for mixers
 
         Button addMixer = (Button)findViewById(R.id.mixersButton);
         addMixer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(BoozeChoose.this,ListIngredients.class);
                 i.putExtra("type","mixers");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        // button listener for liquor
+
+        Button addLiquor = (Button)findViewById(R.id.liquorButton);
+        addLiquor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(BoozeChoose.this,ListIngredients.class);
+                i.putExtra("type","liquor");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        // button listener for liqueur
+
+        Button addLiqueur = (Button)findViewById(R.id.liqueurButton);
+        addLiqueur.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(BoozeChoose.this,ListIngredients.class);
+                i.putExtra("type","liqueur");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        // button listener for misc
+
+        Button addMisc = (Button)findViewById(R.id.miscButton);
+        addMisc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(BoozeChoose.this,ListIngredients.class);
+                i.putExtra("type","misc");
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
             }
