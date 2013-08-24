b97a920154815d0ba3ddbc191f12c89f932e475d
Whitney Champion <whitney.champion@sparcedge.com>
2013-08-23 21:16:20 -0400

    setup hello world

---
+++ gen/com/sparc/BoozeChoose/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sparc.BoozeChoose;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/sparc/BoozeChoose/BoozeChoose.java
@@ -0,0 +1,15 @@
+package com.sparc.BoozeChoose;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoozeChoose extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
