4fe4bc5861393b66ba77e5cf6166b1e34d5499ed
Stephen Quick <squickone@gmail.com>
2013-08-23 22:33:17 -0400

    splash

---
+++ src/com/sparc/BoozeChoose/Adapter/IngredientAdapter.java
@@ -0,0 +1,11 @@
+package com.sparc.BoozeChoose.Adapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Whitney Champion
+ * Date: 8/23/13
+ * Time: 9:48 PM
+ * Description:
+ */
+public class IngredientAdapter {
+}
