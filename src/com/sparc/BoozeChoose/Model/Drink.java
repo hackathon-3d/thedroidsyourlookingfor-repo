b81fc55f8390145af8e6cbfe234744c712812dd7
Whitney Champion <whitney.champion@sparcedge.com>
2013-08-23 21:38:54 -0400

    drink model

---
+++ src/com/sparc/BoozeChoose/Model/Drink.java
@@ -0,0 +1,42 @@
+package com.sparc.BoozeChoose.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Whitney Champion
+ * Date: 8/23/13
+ * Time: 9:37 PM
+ * Description:
+ */
+public class Drink {
+
+    private String id;
+    private String name;
+    private String ingredients;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+       
+
+}
