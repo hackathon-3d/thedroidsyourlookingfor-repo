6335682083561b0b70dabef92fba9f8f18c1c61c
Stephen Quick <squickone@gmail.com>
2013-08-24 04:55:54 -0400

    initial check in of recipe page

---
+++ src/com/sparc/BoozeChoose/Activity/Recipe.java
@@ -0,0 +1,17 @@
+package com.sparc.BoozeChoose.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: stephen quick
+ * Date: 8/24/13
+ * Time: 4:39 AM
+ */
+public class Recipe extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
