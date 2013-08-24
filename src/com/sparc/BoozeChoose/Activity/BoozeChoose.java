faad4f11623a1562652c4c7baea10b50dd7aa2e1
Whitney Champion <whitney.champion@sparcedge.com>
2013-08-23 23:45:28 -0400

    working on db adapter

--- src/com/sparc/BoozeChoose/Activity/BoozeChoose.java
+++ src/com/sparc/BoozeChoose/Activity/BoozeChoose.java
@@ -1,10 +1,18 @@
 package com.sparc.BoozeChoose.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.sparc.BoozeChoose.Adapter.DatabaseAdapter;
 import com.sparc.BoozeChoose.R;
 
+import java.io.IOException;
+
 public class BoozeChoose extends Activity {
+
+    public DatabaseAdapter myDbHelper;
     /**
      * Called when the activity is first created.
      */
@@ -12,5 +20,28 @@ public class BoozeChoose extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // set up db
+
+        myDbHelper = new DatabaseAdapter(this);
+
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+
+        // button listener for contests
+
+        Button addMixer = (Button)findViewById(R.id.mixersButton);
+        addMixer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(BoozeChoose.this,ListIngredients.class);
+                i.putExtra("type","mixers");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- src/com/sparc/BoozeChoose/Adapter/IngredientAdapter.java
+++ src/com/sparc/BoozeChoose/Adapter/IngredientAdapter.java
@@ -1,5 +1,15 @@
 package com.sparc.BoozeChoose.Adapter;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.view.*;
+import android.widget.*;
+import com.sparc.BoozeChoose.Model.Ingredient;
+import com.sparc.BoozeChoose.R;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Whitney Champion
@@ -7,5 +17,58 @@ package com.sparc.BoozeChoose.Adapter;
  * Time: 9:48 PM
  * Description:
  */
-public class IngredientAdapter {
+public class IngredientAdapter extends ArrayAdapter<Ingredient> {
+
+    Context context;
+    int layoutResourceId;
+    Ingredient data[] = null;
+
+    public IngredientAdapter(Context context, int layoutResourceId, Ingredient[] data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final IngredientHolder holder;
+        View row = convertView;
+
+        if ( row == null )
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new IngredientHolder();
+            holder.name = (TextView) row.findViewById(R.id.ingredientName);
+            row.setTag(holder);
+
+
+        } else {
+            holder = (IngredientHolder)row.getTag();
+        }
+
+        final Ingredient ingredient = data[position];
+
+
+        // if ingredients in list, populate data
+        if (ingredient.getName() != null) {
+
+            // set name
+            holder.name.setText(ingredient.getName());
+
+
+
+        } else {
+            holder.name.setText("No ingredients found");
+        }
+
+        return row;
+    }
+
+    static class IngredientHolder {
+        TextView _id;
+        TextView name;
+    }
 }
