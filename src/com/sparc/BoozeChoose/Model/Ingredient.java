c9273cc98bdca7f9589e8eae87a9ed798dc7e467
Whitney Champion <whitney.champion@sparcedge.com>
2013-08-23 22:08:53 -0400

    db update, ingredient model, ingredient adapter

---
+++ src/com/sparc/BoozeChoose/Model/Ingredient.java
@@ -0,0 +1,32 @@
+package com.sparc.BoozeChoose.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Whitney Champion
+ * Date: 8/23/13
+ * Time: 10:05 PM
+ * Description:
+ */
+public class Ingredient {
+
+    private String _id;
+    private String name;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
